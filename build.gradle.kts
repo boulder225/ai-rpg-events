ebf308768b6b87996daac73c848daaa287f98274
Enrico <enrico@MacBookPro.box>
2025-07-09 18:00:21 +0200

    chore: add 'stage' task to build.gradle.kts for Heroku deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,18 @@ tasks.withType<JavaExec> {
     jvmArgs("--enable-preview")
 }
 
+tasks.register("stage") {
+    dependsOn("installDist")
+}
+
 dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    
+    // Logging
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("ch.qos.logback:logback-core:1.4.14")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
     
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation("org.assertj:assertj-core:3.24.2")
